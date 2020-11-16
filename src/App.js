e1f89fe4518c80eff9991e2485566aafc72b39ae
mariosknl <mariosknl@outlook.com>
2020-11-16 12:54:55 +0200

    created circles and button for updating size and filter data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
+import { select } from "d3";
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
   const svgRef = useRef();
-  return <svg ref={svgRef}></svg>;
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", (value) => value)
+      .attr("cx", (value) => value * 2)
+      .attr("cy", (value) => value * 2)
+      .attr("stroke", "red");
+  }, [data]);
+  return (
+    <>
+      <svg ref={svgRef}></svg>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter data
+      </button>
+    </>
+  );
 }
 
 export default App;
